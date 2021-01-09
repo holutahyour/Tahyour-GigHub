1bd85e1a03a5671e0fb5e1961c78dc69d1c76ca8
Onibudo Victor <westernflow15@gmail.com>
2021-01-09 09:43:27 +0100

    Implementing validation

---
+++ ViewModel/ValidTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace GigHub.ViewModel
+{
+    public class ValidTime : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var isValid = DateTime.TryParseExact(Convert.ToString(value),
+                "HH:mm",
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.None,
+                out DateTime dateTime);
+
+            return (isValid);
+        }
+    }
+}
